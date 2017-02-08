cd8a0924f9c19e14ef596f8dc1df42048d743ad6
chilichilimo <afshar.m.95@gmail.com>
2017-02-08 16:26:07 +0000

    adding skeleton

--- src/main/java/com/expedia/www/Main.java
+++ src/main/java/com/expedia/www/Main.java
@@ -1,9 +1,39 @@
 package com.expedia.www;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner scanner = new Scanner(System.in);
+
     	System.out.println("Welcome to the Expedia challenge");
+
+        System.out.println("Please choose:");
+        System.out.println("1. Edit Website");
+        System.out.println("2. Edit Report");
+        System.out.println("3. Exit");
+
+        int userInput = scanner.nextInt();
+
+        switch (userInput){
+            case 1:
+                editWebsite();
+                break;
+            case 2:
+                editReport();
+                break;
+            case 3:
+                return;
+        }
+
+
+    }
+    static void editWebsite(){
+
+    }
+
+    static void editReport(){
+
     }
 }
